dbc64ea8fa19446d7539572f802d8f4a361e4bfd
gnubufferoverflows <6243646+gnubufferoverflows@users.noreply.github.com>
2025-04-21 18:48:16 -0700

    set status

--- Program.cs
+++ Program.cs
@@ -379,7 +379,7 @@ public class Program {
         {
             await ResetCommands();
         }
-        await _client.SetCustomStatusAsync("I love democracy");
+        await _client.SetGameAsync("to your votes", type: ActivityType.Listening);
     }
 
     private static async Task Log(LogMessage message)
